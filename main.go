0778909d2601f82ff05ac1968d61bf268a982520
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-16 22:17:36 +0200

    moves to kinesis

--- main.go
+++ main.go
@@ -2,139 +2,89 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 
 	"strings"
 
-	"errors"
+	"fmt"
 
-	"encoding/base64"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/bwmarrin/discordgo"
-	"github.com/vmihailenco/msgpack"
+	"github.com/json-iterator/go"
 )
 
 var (
-	Token       string
-	SqsQueueUrl string
-	Svc         *sqs.SQS
-	Dg          *discordgo.Session
+	Token    string
+	Dg       *discordgo.Session
+	InitAt   time.Time
+	HandleAt time.Time
 )
 
 func init() {
-	// Parse command line flags (-t DISCORD_BOT_TOKEN -sqs SQS_QUEUE_URL)
+	var err error
+	// set init time
+	InitAt = time.Now()
+	// Parse command line flags (-t DISCORD_BOT_TOKEN)
 	flag.StringVar(&Token, "t", "", "Discord Bot Token")
-	flag.StringVar(&SqsQueueUrl, "sqs", "", "Amazon SQS Queue URL")
 	flag.Parse()
-	// overwrite with environment variables if set DISCORD_BOT_TOKEN=… SQS_QUEUE_URL=…
+	// overwrite with environment variables if set DISCORD_BOT_TOKEN=…
 	if os.Getenv("DISCORD_BOT_TOKEN") != "" {
 		Token = os.Getenv("DISCORD_BOT_TOKEN")
 	}
-	if os.Getenv("SQS_QUEUE_URL") != "" {
-		SqsQueueUrl = os.Getenv("SQS_QUEUE_URL")
-	}
-}
-
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var err error
-	// setup Amazon Session
-	fmt.Println("connecting to Amazon SQS, URL:", SqsQueueUrl)
-	awsSession := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	// setup Amazon SQS queue
-	Svc = sqs.New(awsSession)
-
 	// create a new Discordgo Bot Client
 	fmt.Println("connecting to Discord, Token Length:", len(Token))
 	Dg, err = discordgo.New("Bot " + Token)
 	if err != nil {
-		fmt.Println("error creating discord session,", err.Error())
-		return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-			errors.New("error creating discord session: " + err.Error())
+		panic(err)
 	}
-
-	// Receive last Amazon SQS message
-	result, err := Svc.ReceiveMessage(&sqs.ReceiveMessageInput{
-		QueueUrl: aws.String(SqsQueueUrl),
-		AttributeNames: aws.StringSlice([]string{
-			"SentTimestamp",
-		}),
-		MaxNumberOfMessages: aws.Int64(1),
-		MessageAttributeNames: aws.StringSlice([]string{
-			"All",
-		}),
-		WaitTimeSeconds: aws.Int64(60),
-	})
-	if err != nil {
-		fmt.Println("unable to receive message from queue:", err.Error())
-		return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-			errors.New("unable to receive message from queue: " + err.Error())
-	}
-
-	if result == nil || result.Messages == nil || len(result.Messages) <= 0 {
-		fmt.Println("received nothing from queue")
-		return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-			errors.New("received nothing from queue")
-	}
-
-	// check if Message is valid
-	receivedEvent := result.Messages[0]
-
-	if receivedEvent.MessageAttributes == nil {
-		fmt.Println("received message with no event type from queue")
-		return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-			errors.New("received message with no event type from queue")
-	}
-
-	if eventType, ok := receivedEvent.MessageAttributes["EventType"]; !ok || eventType == nil || eventType.String() == "" {
-		fmt.Println("received message with no event type from queue")
-		return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-			errors.New("received message with no event type from queue")
-	}
-
-	msgpackData, err := base64.StdEncoding.DecodeString(*receivedEvent.Body)
-	if err != nil {
-		fmt.Println("unable to base64 decode the message data")
-		return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-			errors.New("unable to base64 decode the message data")
-	}
-
-	// match and unpack received event
-	switch receivedEvent.MessageAttributes["EventType"].String() {
-	case "discordgo.MessageCreate":
-		var m *discordgo.MessageCreate
-		err = msgpack.Unmarshal(msgpackData, &m)
-		if err != nil {
-			fmt.Println("error unpacking event:", err.Error())
-			return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-				errors.New("error unpacking event: " + err.Error())
-		}
-		err = MessageCreate(m)
-		if err != nil {
-			fmt.Println("error processing discordgo.MessageCreate:", err.Error())
-			return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-				errors.New("error processing discordgo.MessageCreate: " + err.Error())
-		}
-	default:
-		fmt.Println("received invalid event type:", receivedEvent.MessageAttributes["EventType"].String())
-		return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 500},
-			errors.New("received invalid event type: " + receivedEvent.MessageAttributes["EventType"].String())
-	}
-
-	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
 
-func MessageCreate(m *discordgo.MessageCreate) (err error) {
+func Handler(request events.KinesisEvent) (events.APIGatewayProxyResponse, error) {
+	var err error
+	// set handle time
+	HandleAt = time.Now()
+
+	if request.Records == nil || len(request.Records) <= 0 {
+		fmt.Println("received nothing from kinesis")
+		return events.APIGatewayProxyResponse{
+				Body:       "",
+				StatusCode: 200,
+			},
+			nil
+	}
+
+	var m discordgo.MessageCreate
+
+	err = jsoniter.Unmarshal(request.Records[0].Kinesis.Data, &m)
+	if err != nil {
+		fmt.Println("error unpacking event:", err.Error())
+		return events.APIGatewayProxyResponse{
+				Body:       "",
+				StatusCode: 200,
+			},
+			nil
+	}
+
+	err = MessageCreate(m)
+	if err != nil {
+		fmt.Println("error processing discordgo.MessageCreate:", err.Error())
+		return events.APIGatewayProxyResponse{
+				Body:       "",
+				StatusCode: 200,
+			},
+			nil
+	}
+
+	return events.APIGatewayProxyResponse{Body: "", StatusCode: 200}, nil
+}
+
+func MessageCreate(m discordgo.MessageCreate) (err error) {
 	// respond "pong!" to "ping"
 	if strings.ToLower(strings.TrimSpace(m.Content)) == "ping" {
-		_, err = Dg.ChannelMessageSend(m.ChannelID, "pong!")
+		_, err = Dg.ChannelMessageSend(m.ChannelID, "pong!\nInit at "+InitAt.Format(time.StampNano)+"\nHandler at "+HandleAt.Format(time.StampNano))
 		if err != nil {
 			return err
 		}
