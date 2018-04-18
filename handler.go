3976036ca2302f9cad5f885821fb44c725983043
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-19 01:55:51 +0200

    measures unmarshal time

--- handler.go
+++ handler.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/json-iterator/go"
 
+	"fmt"
+	"time"
+
 	"gitlab.com/project-d-collab/dhelpers"
 )
 
@@ -18,11 +21,14 @@ func (handler lambdaHandler) Invoke(_ context.Context, payload []byte) ([]byte, 
 
 func newHandler(_ interface{}) lambdaHandler {
 	return func(_ context.Context, payload []byte) ([]byte, error) {
+		unmarshalStart := time.Now()
 		var container dhelpers.EventContainer
 		err := jsoniter.Unmarshal(payload, &container)
 		if err != nil {
 			return nil, err
 		}
+		// benchmark
+		fmt.Println("unmarshal took", time.Now().Sub(unmarshalStart).String())
 
 		err = Handler(container)
 		return nil, err
