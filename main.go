278e44b83707cffa9f0ac3d9035a387228d4104f
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-22 15:47:39 +0200

    implements dhelpers LambdaHandler

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"os"
 
 	"fmt"
@@ -11,11 +10,11 @@ import (
 	"strconv"
 	"strings"
 
-	"errors"
-
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/bwmarrin/discordgo"
 	"gitlab.com/project-d-collab/dhelpers"
+	"gitlab.com/project-d-collab/dhelpers/cache"
+	"gitlab.com/project-d-collab/dhelpers/components"
 )
 
 var (
@@ -30,128 +29,103 @@ func init() {
 	// set init time
 	initAt = time.Now()
 	var err error
-	// Parse command line flags (-t DISCORD_BOT_TOKEN -discord-endpoint DISCORD_ENDPOINT)
-	flag.StringVar(&token, "t", "", "Discord Bot Token")
-	flag.StringVar(&discordEndpoint, "discord-endpoint", "https://discordapp.com/", "Discord Endpoint URL")
-	flag.Parse()
-	// overwrite with environment variables if set DISCORD_BOT_TOKEN=… DISCORD_EDNPOINT=…
-	if os.Getenv("DISCORD_BOT_TOKEN") != "" {
-		token = os.Getenv("DISCORD_BOT_TOKEN")
-	}
-	if os.Getenv("DISCORD_ENDPOINT") != "" {
-		discordEndpoint = os.Getenv("DISCORD_ENDPOINT")
-	}
+	// read environment variables if set DISCORD_BOT_TOKEN=… DISCORD_ENDPOINT=…
+	token = os.Getenv("DISCORD_BOT_TOKEN")
+	discordEndpoint = os.Getenv("DISCORD_ENDPOINT")
+	// init components
+	dhelpers.CheckErr(err)
+	components.InitLogger("lambda/pong")
+	err = components.InitSentry()
+	dhelpers.CheckErr(err)
 	// create a new Discordgo Bot Client
 	dhelpers.SetDiscordEndpoints(discordEndpoint)
 	fmt.Println("set Discord Endpoint API URL to", discordgo.EndpointAPI)
 	fmt.Println("connecting to Discord, Token Length:", len(token))
 	dg, err = discordgo.New("Bot " + token)
-	if err != nil {
-		panic(err)
-	}
+	dhelpers.CheckErr(err)
 
 	initFinishedAt = time.Now()
 }
 
 // Handler is the lambda entry point when event is triggered
-func Handler(container dhelpers.EventContainer) error {
+func Handler(event dhelpers.EventContainer) {
 	// benchmark
 	handlerStart := time.Now()
 	defer func() {
-		fmt.Println("handler took", time.Since(handlerStart).String())
+		cache.GetLogger().Infoln("handler took", time.Since(handlerStart).String())
+	}()
+
+	switch event.Type {
+	case dhelpers.MessageCreateEventType:
+
+		switch event.Args[0] {
+		case "ping", "pong":
+			ping(event)
+		}
+	}
+}
+
+// MessageCreate is triggered when a MessageCreate event has been received
+func ping(container dhelpers.EventContainer) {
+	// benchmark
+	messageCreateStart := time.Now()
+	defer func() {
+		cache.GetLogger().Infoln("messagecreate took", time.Since(messageCreateStart).String())
 	}()
 
 	var err error
 
-	switch container.Type {
-	case dhelpers.MessageCreateEventType:
-		err = MessageCreate(handlerStart, container)
-		if err != nil {
-			return errors.New("error processing " + string(container.Type) + ": " + err.Error())
-		}
-	}
-
-	return nil
-}
-
-// MessageCreate is triggered when a MessageCreate event has been received
-func MessageCreate(handleAt time.Time, container dhelpers.EventContainer) (err error) {
-	// benchmark
-	messageCreateStart := time.Now()
-	defer func() {
-		fmt.Println("messagecreate took", time.Since(messageCreateStart).String())
-	}()
-
-	if len(container.Args) < 0 {
-		return nil
-	}
-
-	// respond "pong!" to "ping"
-	switch container.Args[0] {
-	case "ping-myself":
-		_, err = dg.ChannelMessageSend(container.MessageCreate.ChannelID, "/ping")
-		if err != nil {
-			return err
-		}
-		return nil
-	case "ping":
-		_, err = dg.ChannelMessageSendComplex(container.MessageCreate.ChannelID, &discordgo.MessageSend{
-			Embed: &discordgo.MessageEmbed{
-				Title:     "Pong!",
-				Timestamp: time.Now().Format(time.RFC3339),
-				Footer: &discordgo.MessageEmbedFooter{
-					Text:    "requested by " + container.MessageCreate.Author.Username + "#" + container.MessageCreate.Author.Discriminator,
-					IconURL: container.MessageCreate.Author.AvatarURL("64"),
+	_, err = dg.ChannelMessageSendComplex(container.MessageCreate.ChannelID, &discordgo.MessageSend{
+		Embed: &discordgo.MessageEmbed{
+			Title:     "Pong!",
+			Timestamp: time.Now().Format(time.RFC3339),
+			Footer: &discordgo.MessageEmbedFooter{
+				Text:    "requested by " + container.MessageCreate.Author.Username + "#" + container.MessageCreate.Author.Discriminator,
+				IconURL: container.MessageCreate.Author.AvatarURL("64"),
+			},
+			/*
+				Author: &discordgo.MessageEmbedAuthor{
+					Name:    container.BotUser.Username + "#" + container.BotUser.Discriminator,
+					IconURL: container.BotUser.AvatarURL("64"),
 				},
-				/*
-					Author: &discordgo.MessageEmbedAuthor{
-						Name:    container.BotUser.Username + "#" + container.BotUser.Discriminator,
-						IconURL: container.BotUser.AvatarURL("64"),
-					},
-				*/
-				Fields: []*discordgo.MessageEmbedField{
-					{
-						Name:   "Init",
-						Value:  "At " + initAt.Format(time.StampNano) + "\nTook " + initFinishedAt.Sub(initAt).String(),
-						Inline: false,
-					},
-					{
-						Name:   "Handler",
-						Value:  handleAt.Format(time.StampNano),
-						Inline: false,
-					},
-					{
-						Name:   "Gateway => Lambda",
-						Value:  handleAt.Sub(container.ReceivedAt).String(),
-						Inline: false,
-					},
-					{
-						Name:   "Gateway Uptime",
-						Value:  time.Since(container.GatewayStarted).String() + "\nStarted at " + strconv.FormatInt(container.GatewayStarted.Unix(), 10),
-						Inline: false,
-					},
-					{
-						Name:   "Args",
-						Value:  "`" + strings.Join(container.Args, "`, `") + "`",
-						Inline: false,
-					},
-					{
-						Name:   "Used Prefix",
-						Value:  container.Prefix,
-						Inline: false,
-					},
+			*/
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "Init",
+					Value:  "At " + initAt.Format(time.StampNano) + "\nTook " + initFinishedAt.Sub(initAt).String(),
+					Inline: false,
+				},
+				{
+					Name:   "Handler",
+					Value:  messageCreateStart.Format(time.StampNano),
+					Inline: false,
+				},
+				{
+					Name:   "Gateway => Lambda",
+					Value:  messageCreateStart.Sub(container.ReceivedAt).String(),
+					Inline: false,
+				},
+				{
+					Name:   "Gateway Uptime",
+					Value:  time.Since(container.GatewayStarted).String() + "\nStarted at " + strconv.FormatInt(container.GatewayStarted.Unix(), 10),
+					Inline: false,
+				},
+				{
+					Name:   "Args",
+					Value:  "`" + strings.Join(container.Args, "`, `") + "`",
+					Inline: false,
+				},
+				{
+					Name:   "Used Prefix",
+					Value:  container.Prefix,
+					Inline: false,
 				},
 			},
-		})
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-	return nil
+		},
+	})
+	dhelpers.CheckErr(err)
 }
 
 func main() {
-	wrappedHandler := newHandler(Handler)
-	lambda.StartHandler(wrappedHandler)
+	lambda.StartHandler(dhelpers.NewLambdaHandler("lambda/pong", Handler))
 }
