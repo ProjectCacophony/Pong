e705b4d2212d870734f2e002319bfe191ad84d20
Jordan Kueh <jordan@zerocu.be>
2018-06-10 16:35:21 +1000

    Attempting to deploy via Terraform, added go build steps and temporarily protecting the gitlab-ci branch so that we can use the deploy runners

--- .deployment/main.tf
+++ .deployment/main.tf
@@ -1,6 +1,10 @@
-provider "aws" {
+terraform {
   backend "s3" {
     bucket = "cacophony-terraform"
+    key    = "lambda-pong"
     region = "us-east-1"
   }
+}
+provider "aws" {
+  region = "us-east-1"
 }
