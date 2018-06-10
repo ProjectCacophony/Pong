7381573248ef1e0866d5e877be594a8da9ed2c0e
Jordan Kueh <jordan@zerocu.be>
2018-06-10 18:05:07 +1000

    Adding Terraform variables, using it to define the environment slug - Also implementing it as part of the lambda function name

--- .deployment/lambda.tf
+++ .deployment/lambda.tf
@@ -6,13 +6,13 @@ data "archive_file" "pkg" {
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name               = "pong_lambda_role"
+  name               = "pong_lambda_role_${var.env_slug}"
   assume_role_policy = "${file("lambda_assumerolepolicy.json")}"
 }
 
 resource "aws_lambda_function" "lambda_pong" {
   filename         = "${data.archive_file.pkg.output_path}"
-  function_name    = "pong"
+  function_name    = "pong-${var.env_slug}"
   role             = "${aws_iam_role.lambda_role.arn}"
   handler          = "${data.archive_file.pkg.source_file}"
   source_code_hash = "${base64sha256(file("${data.archive_file.pkg.output_path}"))}"
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "lambda_pong" {
 
   environment {
     variables = {
-      Environment = "staging"
+      Environment = "${var.env_slug}"
     }
   }
 }
