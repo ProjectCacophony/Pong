7e99225f2cae780a63c272cad3c5cef3d9e71c35
Jordan Kueh <jordan@zerocu.be>
2018-06-10 16:40:24 +1000

    Terraform fmt - [skip ci]

--- .deployment/lambda.tf
+++ .deployment/lambda.tf
@@ -1,22 +1,23 @@
 # Build the archive file to send to lambda
 data "archive_file" "pkg" {
-  type = "zip"
+  type        = "zip"
   source_file = "pong"
   output_path = "pong_lambda_pkg.zip"
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name = "pong_lambda_role"
+  name               = "pong_lambda_role"
   assume_role_policy = "${file("lambda_assumerolepolicy.json")}"
 }
 
 resource "aws_lambda_function" "lambda_pong" {
-  filename = "${data.archive_file.pkg.output_path}"
-  function_name = "pong"
-  role = "${aws_iam_role.lambda_role.arn}"
-  handler = "${data.archive_file.pkg.source_file}"
+  filename         = "${data.archive_file.pkg.output_path}"
+  function_name    = "pong"
+  role             = "${aws_iam_role.lambda_role.arn}"
+  handler          = "${data.archive_file.pkg.source_file}"
   source_code_hash = "${base64sha256(file("${data.archive_file.pkg.output_path}"))}"
-  runtime = "go1.x"
+  runtime          = "go1.x"
+
   environment {
     variables = {
       Environment = "staging"
